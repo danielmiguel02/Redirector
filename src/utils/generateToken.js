711279ae8a5116874c6dbb36b069012144d541d8
Daniel Miguel <danielmiguel.dev02@gmail.com>
2026-02-23 13:02:26 -0500

    feat: started implemented auth feature by creating support methods like generateToken, verifyToken, hashPassword, checkPassword

---
+++ src/utils/generateToken.js
@@ -0,0 +1,14 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const generateToken = (payload) => {
+    return jwt.sign(
+        payload,
+        process.env.JWT_SECRET,
+        {
+            expiresIn: process.env.JWT_EXPIRES_IN || "7d",
+        }
+    );
+};
