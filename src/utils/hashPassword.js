a0d34634c42c526d9437e71927fe2834df6f6741
Daniel Miguel <danielmiguel.dev02@gmail.com>
2026-02-25 13:46:11 -0500

    fix: when generating a salt it was calling method bcrypt.salt instead of bcrypt.genSalt, also bcrypt.compare had spelling mistake.

--- src/utils/hashPassword.js
+++ src/utils/hashPassword.js
@@ -1,7 +1,7 @@
 import bcrypt from 'bcryptjs';
 
 export const hashPassword = async (password) => {
-    const salt = await bcrypt.salt(10);
+    const salt = await bcrypt.genSalt(10);
 
     const hashedPassword = await bcrypt.hash(password, salt);
 
@@ -10,7 +10,7 @@ export const hashPassword = async (password) => {
 
 export const checkPassword = async (password, hashedPassword) => {
     try {
-        const match = await bcrypt.compare(password, hashPassword);
+        const match = await bcrypt.compare(password, hashedPassword);
         if (match) return match;
 
         console.log('Invalid password or email');
