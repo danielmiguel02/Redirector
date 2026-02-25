7e393146a88f7c49bb667ebf70b10eb5436f3f4f
Daniel Miguel <danielmiguel.dev02@gmail.com>
2026-02-25 13:54:38 -0500

    feat: implemented errorMiddlware to take care of global error handling

---
+++ src/middlewares/errorMiddleware.js
@@ -0,0 +1,24 @@
+import { ZodError } from "zod";
+
+export const errorMiddleware = (err, req, res, next) => {
+    console.error(err);
+
+    if (err instanceof ZodError) {
+        return res.status(400).json({
+            type: "ValidationError",
+            errors: err.errors,
+        });
+    }
+
+    if (err.statusCode) {
+        return res.status(err.statusCode).json({
+            type: "ApiError",
+            message: err.message,
+        });
+    }
+
+    return res.status(500).json({
+        type: "ServerError",
+        message: "Internal Server Error",
+    });
+};
