abe5ad909af7c21276d376c32fbfb2575bde382a
Daniel Miguel <danielmiguel.dev02@gmail.com>
2026-03-02 12:35:05 -0500

    update: updated url routes to take url redirector route

--- src/routes/urlRoutes.js
+++ src/routes/urlRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { authMiddleware } from '../middlewares/authMiddleware.js';
-import { createUrlController } from '../controllers/urlController.js';
+import { createUrlController, urlRedirectController } from '../controllers/urlController.js';
 
 const router = express.Router();
 
 router.post("/create", authMiddleware, createUrlController);
+router.post("/:code", urlRedirectController);
 
 export default router;
