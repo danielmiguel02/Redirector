c38a2984c00b50e8f563d92c7a3571968f8a8a4f
Daniel Miguel <danielmiguel.dev02@gmail.com>
2026-03-02 11:46:20 -0500

    update: urlRefirectController needs to send ip, userAgent, and referer to service

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -22,8 +22,18 @@ const createUrlController = async (req, res, next) => {
 
 const urlRedirectController = async (req, res, next) => {
     try {
+        const ip = req.headers["x-forwarded-for"]?.split(",")[0] ||
+                   req.socket.remoteAddress;
+
+        const userAgent = req.headers["user-agent"];
+
+        const referer = req.headers["referer"] || null;
+
         const result = await urlRedirectService({
-            code: req.params.code
+            code: req.params.code,
+            ip: ip,
+            userAgent: userAgent,
+            referer: referer,
         });
 
         return res.status(200).json({
