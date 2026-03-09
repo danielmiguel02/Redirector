1b671919f0644ff7e94809206c00a29239656551
Daniel Miguel <danielmiguel.dev02@gmail.com>
2026-03-09 12:16:06 -0400

    feat: implemented rateLimitService to take care of user rate limits when creating urls

---
+++ src/services/rateLimitService.js
@@ -0,0 +1,46 @@
+import { ApiError } from '../utils/ApiError.js';
+import { createRateLimitRepository, updateRateLimitRepository, rateLimitRepository } from '../repositories/rateLimitRepository.js';
+
+const RATE_LIMIT = 5; // max attempts
+const WINDOW_TIME = 60 * 1000; // 1 minute
+
+const rateLimitService = async (ip) => {
+    if (!ip) {
+        throw new ApiError(401, "No ip to create rate limit")
+    }
+
+    const limit = await rateLimitRepository(ip);
+    const now = Date.now();
+
+    if (!limit) {
+        await createRateLimitRepository({
+            ip: ip,
+            count: 1,
+            lastHit: now
+        });
+        return;
+    }
+
+    const timeSinceLastHit = now - limit.lastHit;
+
+    if (timeSinceLastHit > WINDOW_TIME) {
+        await updateRateLimitRepository({
+            ip: ip,
+            count: 1,
+            lastHit: now,
+        });
+        return;
+    }
+
+    if (limit.count >= RATE_LIMIT) {
+        throw new ApiError(429, "Url creation is limited to 5 per minute");
+    }
+
+    await updateRateLimitRepository({
+        ip: ip,
+        count: limit.count + 1,
+        lastHit: now,
+    });
+};
+
+export { rateLimitService };
