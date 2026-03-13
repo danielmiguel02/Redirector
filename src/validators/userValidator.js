924b9c2a4a8abd86b75ef8dcbbf04d852a3771ad
Daniel Miguel <danielmiguel.dev02@gmail.com>
2026-03-13 12:37:58 -0400

    update: removed checking length of password when loggin in

--- src/validators/userValidator.js
+++ src/validators/userValidator.js
@@ -24,8 +24,4 @@ export const loginUserSchema = z.object({
   email: z
     .string()
     .email({ message: "Invalid email format" }),
-
-  password: z
-    .string()
-    .min(8, { message: "Password must be at least 8 characters" }),
 });
