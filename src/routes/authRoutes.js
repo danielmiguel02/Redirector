748e37fe4a712fb3f77938e613c4e839efa7f8db
Daniel Miguel <danielmiguel.dev02@gmail.com>
2026-03-13 12:12:32 -0400

    update: updating authRoutes to take errorMiddleware

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import { registerUserController, loginUserController, logoutUserController } from '../controllers/authController.js';
+import { errorMiddleware } from '../middlewares/errorMiddleware.js';
 
 const router = express.Router();
 
 router.post("/register", registerUserController);
 router.post("/login", loginUserController);
 router.post("/logout", logoutUserController);
+
+router.use(errorMiddleware);
 
 export default router;
